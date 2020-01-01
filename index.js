0e0b6b8f1a1afafc5bfaaaf6503fb4b7795e165e
DPS <smith.dp@gmail.com>
2020-01-01 21:26:15 +0000

    update to use elasticache

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const routes = require('./routes')
 async function startServer () {
   const server = Hapi.Server({
     port: process.env.PORT || 3000
+    cache: [
+      {
+        name: 'redis',
+        provider: {
+          constructor: require('@hapi/catbox-redis'),
+          options: {
+            partition: 'cache',
+            host: 'pizza-cluster.ptdfbp.0001.use1.cache.amazonaws.com'
+          }
+        }
+      }
+    ]
   })
 
   await plugins.register(server)
