6fb21c0591eb3bd21a5dc39e5cd2d26e69f990cb
DPS <smith.dp@gmail.com>
2020-01-01 16:17:56 +0000

    connect pizza luvrs to postgres

---
+++ data/pizzaStore.js
@@ -0,0 +1,39 @@
+const Sequalize = require("sequalize");
+
+const database = "pizza_luvrs";
+const host = "pizza-db.cua7ozwnfxpl.us-east-1.rds.amazonaws.com";
+const username = "pizza";
+const password = "";
+
+const pgClient = new Sequelize(database, username, password, {
+  host: host,
+  dialect: "postgres"
+});
+
+const Pizza = pgClient.define("pizza", {
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  toppings: {
+    type: Sequelize.STRING
+  },
+  img: {
+    type: Sequelize.STRING
+  },
+  username: {
+    type: Sequelize.STRING
+  },
+  created: {
+    type: Sequelize.BIGINT
+  }
+});
+
+Pizza.sync().then(() => {
+  console.log("postgres connection ready");
+});
+
+module.exports = Pizza;
